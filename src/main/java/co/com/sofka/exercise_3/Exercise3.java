1e42c6bc8e9cbf2c665e4fc59b2385c63946bbd4
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-13 23:32:27 -0500

    Agregando ejercicio 3

---
+++ src/main/java/co/com/sofka/exercise_3/Exercise3.java
@@ -0,0 +1,44 @@
+package co.com.sofka.exercise_3;
+
+import java.util.function.BiFunction;
+import java.util.logging.Logger;
+
+public class Exercise3 {
+
+    private static final Logger log = Logger.getLogger("MyLogger");
+
+    private Double coeficiente;
+    private Double potencia;
+    private Double x;
+    private Double xc;
+    private Double xp;
+    private Double numero;
+
+    public void OperacionesCalculo(Double coeficiente, Double potencia) {
+        this.coeficiente = coeficiente;
+        this.potencia = potencia;
+        this.x = 0.0;
+        this.xc = 0.0;
+        this.xp = 0.0;
+        this.numero = 1.0;
+    }
+
+    public String calcularDerivada() {
+        BiFunction<Double, Double, Double> xc = (coeficiente, potencia) -> coeficiente * potencia;
+        BiFunction<Double, Double, Double> xp = (numero, potencia) -> potencia - numero;
+        return "La derivada es: " + xc.apply(coeficiente, potencia) + "x^" + xp.apply(numero, potencia) + "\n";
+    }
+
+    public String calcularIntegral() {
+        BiFunction<Double, Double, Double> xp = Double::sum;
+        BiFunction<Double, Double, Double> xc = (coeficiente, potencia) -> coeficiente / xp.apply(numero, potencia);
+        return "La Integral es: " + xc.apply(coeficiente, potencia) + "x^" + xp.apply(numero, potencia) + "\n";
+    }
+
+    public static void main(String[] args) {
+        Exercise3 exercise3 = new Exercise3();
+        exercise3.OperacionesCalculo(5.0, 2.0);
+        log.info(exercise3.calcularDerivada());
+        log.info(exercise3.calcularIntegral());
+    }
+}
